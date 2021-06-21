573b0cee92841799df6234b0622ba2c5501083c6
KirillDorokh <kirill.dorokh@gmail.com>
2021-06-21 12:45:37 +0400

    E2_m

--- E2/index.js
+++ E2/index.js
@@ -1,3 +1,11 @@
 let myage = 25;
 
 console.log('Hello world and I\'m ' + myage);
+
+age = prompt('Please enter dog\'s age');
+dogsAge = age * 7;
+alert('My dog\'s age is ' + dogsAge + ' human\'s age')
+
+rub = prompt('plese enter sum of rub');
+usd = rub * 0.014;
+alert(rub + ' rub is ' + usd + ' usd');
