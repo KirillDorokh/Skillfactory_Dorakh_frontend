6f76258832208884887b741bcd5192c68c264e38
KirillDorokh <kirill.dorokh@gmail.com>
2021-06-22 13:29:09 +0400

    E2_3

--- E2/index.js
+++ E2/index.js
@@ -9,3 +9,15 @@ alert('My dog\'s age is ' + dogsAge + ' human\'s age')
 rub = prompt('plese enter sum of rub');
 usd = rub * 0.014;
 alert(rub + ' rub is ' + usd + ' usd');
+
+const a = 1;
+// если b = 2, выполнение программы “провалится” в if
+const b = 2;
+// если b = 3, выполнение программы “провалится” в else
+// const b = 3;
+const result = a + b;
+if (result < 4){
+    console.log("результат меньше 4")
+}else{
+    console.log("результат больше или равен 4")
+}
